0d0f2d04fc51cd4b5abf86b4aa0f7d9cd269db80
narjis avd <avdnarjis@gmail.com>
2026-07-19 09:31:56 +0500

    setting email

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://narjis-fatima:Adobe110!@cluster0.tfwtbck.mongodb.net/gamingHub?retryWrites=true&w=majority';
+  const conn = await mongoose.connect(mongoUri);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB Connection Error: ${error.message}`);
